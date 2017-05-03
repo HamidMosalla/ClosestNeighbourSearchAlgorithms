9a20a5abcb9d02828d6a1f6aa9f57a63d66790bd
Hamid Mosalla <Xellarix@gmail.com>
2017-05-03 09:48:59 +0430

    adds unit test project

---
+++ ClosestNeighbourSearchTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClosestNeighbourSearchTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClosestNeighbourSearchTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ea5710c-4004-4997-9522-6478ccdf8df8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ClosestNeighbourSearchTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace ClosestNeighbourSearchTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+
+            Assert.Equal(2, 2);
+
+        }
+    }
+}
